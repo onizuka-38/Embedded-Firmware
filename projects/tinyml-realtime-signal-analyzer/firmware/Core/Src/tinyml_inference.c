bf85d8ef863005ecc6abecd55b1ad1840ecf2b1a
onizuka-38 <bdh9938@gmail.com>
2026-03-31 11:29:49 +0900

    inference

---
+++ projects/tinyml-realtime-signal-analyzer/firmware/Core/Src/tinyml_inference.c
@@ -0,0 +1,18 @@
+﻿#include "tinyml_inference.h"
+
+#include "tinyml_model_params.h"
+
+int tinyml_predict_label(const float *features, uint32_t feature_count)
+{
+    if (features == 0 || feature_count != TINYML_FEATURE_COUNT) {
+        return -1;
+    }
+
+    float acc = (float)TINYML_BIAS_Q7 * TINYML_WEIGHT_SCALE;
+    for (uint32_t i = 0; i < feature_count; ++i) {
+        const float centered = (features[i] - kTinyMlFeatureMeans[i]) / kTinyMlFeatureStds[i];
+        acc += centered * ((float)kTinyMlWeightsQ7[i] * TINYML_WEIGHT_SCALE);
+    }
+
+    return (acc >= 0.0f) ? 1 : 0;
+}
