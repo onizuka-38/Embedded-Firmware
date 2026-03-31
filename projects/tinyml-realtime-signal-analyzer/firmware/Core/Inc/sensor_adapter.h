55d815a560cad43378633f30f17ceb53246df9bd
onizuka-38 <bdh9938@gmail.com>
2026-03-31 11:43:08 +0900

    adapt

--- projects/tinyml-realtime-signal-analyzer/firmware/Core/Inc/sensor_adapter.h
+++ projects/tinyml-realtime-signal-analyzer/firmware/Core/Inc/sensor_adapter.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+int sensor_read_raw_sample(float *out_sample);
 int sensor_push_sample(float sample);
 int sensor_read_feature_vector(float *features, uint32_t feature_count);
 void sensor_reset(void);
